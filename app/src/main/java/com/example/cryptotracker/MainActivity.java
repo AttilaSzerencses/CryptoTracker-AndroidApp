818e06cffbd5eb935b8583edb0237c1774b96ac8
AttilaSzerencses <szerencses.attila.06@gmail.com>
2022-04-13 01:18:43 +0200

    Implement google login oportunity

--- app/src/main/java/com/example/cryptotracker/MainActivity.java
+++ app/src/main/java/com/example/cryptotracker/MainActivity.java
@@ -11,22 +11,32 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
         private static final String LOG_TAG = MainActivity.class.getName();
         private static final String PREF_KEY = MainActivity.class.getPackage().toString();
         private static final int SECRET_KEY = 99;
+        private static final int RC_SIGN_IN = 123;
 
         EditText userNameET;
         EditText passwordET;
 
         private SharedPreferences preferences;
         private FirebaseAuth mAuth;
+        private GoogleSignInClient mGoogleSignInClient;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
             preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
             mAuth = FirebaseAuth.getInstance();
+
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
+            mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
             Log.i(LOG_TAG, "onCreate");
         }
 
@@ -55,6 +71,38 @@ public class MainActivity extends AppCompatActivity {
                     } else{
                         Log.d(LOG_TAG, "User login fail");
                         Toast.makeText(MainActivity.this, "User login fail: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == RC_SIGN_IN){
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    Log.d(LOG_TAG, "firebaseAuthWithGoogle: "+ account.getId());
+
+                } catch (ApiException e) {
+                    Log.w(LOG_TAG, "Google sign in failed: ", e);
+                }
+            }
+        }
+
+        private void firebaseAuthWithGoogle(String idToken){
+            AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Log.d(LOG_TAG, "signInWithCredential: succes");
+                        startCryptoSite();
+                    } else {
+                        Log.w(LOG_TAG, "signInWithCredential:failure", task.getException());
                     }
                 }
             });
@@ -128,5 +176,8 @@ public class MainActivity extends AppCompatActivity {
             });
     }
 
-
+    public void loginWithGoogle(View view) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 }
