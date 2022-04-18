1f9183f845e39610ea21a364a934483ad031a8de
AttilaSzerencses <szerencses.attila.06@gmail.com>
2022-04-19 00:48:17 +0200

    Notification when save crypto, animation, design

--- app/src/main/java/com/example/cryptotracker/adapter/CurrencyRVAdapter.java
+++ app/src/main/java/com/example/cryptotracker/adapter/CurrencyRVAdapter.java
@@ -1,14 +1,18 @@
 package com.example.cryptotracker.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cryptotracker.NotificationHelper;
 import com.example.cryptotracker.R;
 import com.example.cryptotracker.modul.CurrencyModel;
 
@@ -20,10 +24,13 @@ public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.Cu
     private static DecimalFormat df2 = new DecimalFormat("#.##");
     private ArrayList<CurrencyModel> currencyModals;
     private Context context;
+    private NotificationHelper mNotificationHelper;
+    private int lastPosition = -1;
 
     public CurrencyRVAdapter(ArrayList<CurrencyModel> currencyModals, Context context) {
         this.currencyModals = currencyModals;
         this.context = context;
+        mNotificationHelper = new NotificationHelper(context);
     }
 
     public void filterList(ArrayList<CurrencyModel> filterllist) {
@@ -49,6 +56,12 @@ public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.Cu
         holder.nameTV.setText(modal.getName());
         holder.rateTV.setText("$ " + df2.format(modal.getPrice()));
         holder.symbolTV.setText(modal.getSymbol());
+
+        if(holder.getAdapterPosition() > lastPosition){
+            Animation animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_row);
+            holder.itemView.startAnimation(animation);
+            lastPosition=holder.getAdapterPosition();
+        }
     }
 
     @Override
@@ -64,6 +77,11 @@ public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.Cu
             symbolTV = itemView.findViewById(R.id.idTVSymbol);
             rateTV = itemView.findViewById(R.id.idTVRate);
             nameTV = itemView.findViewById(R.id.idTVName);
+
+            itemView.findViewById(R.id.favorite).setOnClickListener(view -> {
+                //TODO: Crypto eltárolása a profile fül alá
+                mNotificationHelper.send("You're saving " +nameTV.getText()+ " to your saved list!");
+            });
         }
     }
 }
